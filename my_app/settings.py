7054a453d5a056a6cc740ea7bf4d0415200da77f
Sahil Mangotra <sahilmangotra1997@gmail.com>
2017-07-20 21:41:37 +0530

    Models for news made

--- my_app/settings.py
+++ my_app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'bootstrap3',
+    'news',
 ]
 
 MIDDLEWARE = [
--- news/models.py
+++ news/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.core.urlresolvers import reverse
 # Create your models here.
+
+User = get_user_model()
+
+class post(models.Model):
+    user = models.ForeignKey(User,related_name='posts')
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+
+    def __str__(self):
+        return self.name
+
+    def save(self,*args,**kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('news:single',kwargs={'username':self.user.username,'pk':self.pk})
+
+    class Meta():
+        ordering = ['-created_at']
+        unique_together = ['user','message']
